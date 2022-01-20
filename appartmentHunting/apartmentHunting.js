a841cdb5cb4840560be1cfc7adc097a38ace6154
Tony Scalfaro <tscalfaro7@gmail.com>
2022-01-20 12:19:23 -0500

    adding comments

--- appartmentHunting/apartmentHunting.js
+++ appartmentHunting/apartmentHunting.js
@@ -25,7 +25,7 @@
 
 //MY ANSWER
 //SPACE O(BR) || TIME O(BR) WHERE B=# OF BLOCKS AND R=# OF REQS
-//THIS VERSION OF THE SOLUTION IS A TRADEOFF IN SPACE FOR A PERFORMANCE BOOST IN TIME. DEPENDING ON THE NEEDS OF THE CLIENT, YOU WOULD CHOOSE WHICH SOLUTION
+//THIS VERSION OF THE SOLUTION IS A TRADEOFF IN SPACE FOR A PERFORMANCE BOOST IN TIME. DEPENDING ON THE NEEDS OF THE CLIENT, YOU WOULD CHOOSE WHICH SOLUTION. THE OTHER SOLUTION IS NESTED FOR LOOPS RESULTING IN A TIME OF O(B^2R) AND SPACE O(B)
 function apartmentHunting(blocks, reqs) {
       let minDistancesFromBlocks = reqs.map(req => getMinDistances(blocks, req))
       let maxDistancesAtBlocks = getMaxDistancesAtBlocks(blocks, minDistancesFromBlocks)
@@ -35,10 +35,12 @@ function apartmentHunting(blocks, reqs) {
   function getMinDistances(blocks, req){
       let minDistances = new Array(blocks.length)
       let closestReqIdx = Infinity;
+      //GO THROUGH BLOCKS LEFT -> RIGHT, FIND MIN DISTANCE TO REQ 
       for (let i = 0; i < blocks.length; i++){
           if (blocks[i][req]) closestReqIdx = i;
           minDistances[i] = distanceBetween(i, closestReqIdx);
       }
+      //NOW GO THROUGH BLOCKS RIGHT -> LEFT, ADJUST THOSE NUMBERS IF THERE IS A SMALLER DISTANCE
       for (let i = blocks.length - 1; i >= 0; i--){
           if (blocks[i][req]) closestReqIdx = i;
           minDistances[i] = Math.min(minDistances[i], distanceBetween(i, closestReqIdx));
@@ -54,6 +56,12 @@ function apartmentHunting(blocks, reqs) {
   //[0,  1,  0,  1,  0]
   //[1,  0,  0,  1,  2]
   //[1,  0,  1,  2,  3]
+
+  //HOW WE ARE TRYING TO REPRESENT EACH
+//   [0, 1, 1] -columns
+//   [1, 0, 0]
+//   etc.
+  //THEN FIND MAX
 
   function getMaxDistancesAtBlocks(blocks, minDistancesFromBlocks) {
       let maxDistancesAtBlocks = new Array(blocks.length)
