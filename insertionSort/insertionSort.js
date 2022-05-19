2acd2c918e800ee7605efe4cc6aaf9a9e4bb8a38
Tony Scalfaro <tscalfaro7@gmail.com>
2022-05-19 10:41:22 -0400

    insertion sort js

---
+++ insertionSort/insertionSort.js
@@ -0,0 +1,17 @@
+function insertionSort(array) {
+    // Write your code here.
+      for (let i = 1; i < array.length; i++){
+          let j = i;
+          while (j > 0 && array[j] < array[j - 1]){
+              swap(j, j-1, array)
+              j-=1;
+          }
+      }
+      return array
+  }
+  
+  function swap(i, j, array){
+      let temp = array[j];
+      array[j] = array[i];
+      array[i] = temp;
+  }
